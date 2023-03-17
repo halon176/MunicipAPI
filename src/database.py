aac44188effb0ad14a66e0b0f4450490d3e67e2e
halon176 <acc.github@halon.cc>
2023-03-17 13:42:31 +0100

    Initial commit

---
+++ src/database.py
@@ -0,0 +1,8 @@
+import databases
+import sqlalchemy
+
+from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+
+
+metadata = sqlalchemy.MetaData()
+database = databases.Database(f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
