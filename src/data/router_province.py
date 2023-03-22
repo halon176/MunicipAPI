93e645b0aff9be1b22f50aa0bb5253e0beca5fe1
halon176 <acc.github@halon.cc>
2023-03-23 00:01:25 +0100

    added comuni_in_provincia function

--- src/data/router_province.py
+++ src/data/router_province.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 
 from src.auth.router_user import api_key_auth
-from src.data.models import Province, GetProvince
+from src.data.models import Province, GetProvince, GetComuni, Comuni
 
 router = APIRouter(
     prefix="/province",
@@ -22,3 +22,15 @@ async def list_province():
 async def get_provincia(nome: str):
     province = await Province.objects.filter(nome=nome).get()
     return province
+@router.get("/r/{nome}", response_model=List[GetComuni])
+async def comuni_in_provincia(nome: str):
+    province = await Province.objects.filter(nome__iexact=nome).get()
+    comuni = await Comuni.objects.filter(provincia=province.id).all()
+    comuni_list = []
+    for comune in comuni:
+        comune_dict = comune.dict()
+        comune_dict["provincia"] = province.nome
+        comuni_list.append(comune_dict)
+    return comuni_list
+
+
