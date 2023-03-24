b6b1e1df21ea27503e4ac2cd7a4c4df0a09ebb20
halon176 <acc.github@halon.cc>
2023-03-24 15:02:16 +0100

    comment all user's support

--- src/auth/models.py
+++ src/auth/models.py
@@ -5,39 +5,6 @@ import bcrypt
 
 from src.database import metadata, database
 
-
-class User(ormar.Model):
-    class Meta:
-        metadata = metadata
-        database = database
-        tablename = "users"
-
-    id: uuid.UUID = ormar.UUID(primary_key=True, default=uuid.uuid4)
-    username: str = ormar.String(unique=True, max_length=255)
-    email: str = ormar.String(index=True, unique=True, max_length=255)
-    hashed_password: str = ormar.String(max_length=255)
-    is_active: bool = ormar.Boolean(default=False, nullable=False)
-    is_superuser: bool = ormar.Boolean(default=False, nullable=False)
-    created_at: datetime = ormar.DateTime(default=datetime.utcnow)
-
-    @property
-    def password(self):
-        raise AttributeError('La password non può essere letta')
-
-    @password.setter
-    def password(self, plaintext_password):
-        self.hashed_password = bcrypt.hashpw(plaintext_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-    def check_password(self, plaintext_password):
-        return bcrypt.checkpw(plaintext_password.encode('utf-8'), self.hashed_password.encode('utf-8'))
-
-    def check_active(self):
-        if self.is_active:
-            return True
-        else:
-            return False
-
-
 class APIKey(ormar.Model):
     class Meta:
         metadata = metadata
@@ -45,5 +12,39 @@ class APIKey(ormar.Model):
         tablename = "apikey"
 
     apikey: str = ormar.String(primary_key=True, max_length=22)
-    id_user = ormar.UUID(default=uuid.uuid4)
-    created_at: datetime = ormar.DateTime(default=datetime.utcnow)
+    user = ormar.String(max_length=255)
+#    created_at: datetime = ormar.DateTime(default=datetime.utcnow)
+
+# class User(ormar.Model):
+#     class Meta:
+#         metadata = metadata
+#         database = database
+#         tablename = "users"
+#
+#     id: uuid.UUID = ormar.UUID(primary_key=True, default=uuid.uuid4)
+#     username: str = ormar.String(unique=True, max_length=255)
+#     email: str = ormar.String(index=True, unique=True, max_length=255)
+#     hashed_password: str = ormar.String(max_length=255)
+#     is_active: bool = ormar.Boolean(default=False, nullable=False)
+#     is_superuser: bool = ormar.Boolean(default=False, nullable=False)
+#     created_at: datetime = ormar.DateTime(default=datetime.utcnow)
+#
+#     @property
+#     def password(self):
+#         raise AttributeError('La password non può essere letta')
+#
+#     @password.setter
+#     def password(self, plaintext_password):
+#         self.hashed_password = bcrypt.hashpw(plaintext_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+#
+#     def check_password(self, plaintext_password):
+#         return bcrypt.checkpw(plaintext_password.encode('utf-8'), self.hashed_password.encode('utf-8'))
+#
+#     def check_active(self):
+#         if self.is_active:
+#             return True
+#         else:
+#             return False
+
+
+
--- src/auth/router_user.py
+++ src/auth/router_user.py
@@ -8,7 +8,7 @@ from email_validator import validate_email, EmailNotValidError
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.security import APIKeyHeader
 
-from src.auth.models import User, APIKey
+from src.auth.models import APIKey
 
 router = APIRouter(
     prefix="/user",
@@ -16,10 +16,7 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def user_list():
-    userlist = await User.objects.all()
-    return userlist
+
 
 
 X_API_KEY = APIKeyHeader(name='X-API-Key')
@@ -34,52 +31,57 @@ async def api_key_auth(x_api_key: str = Depends(X_API_KEY)):
             detail="Invalid API Key. Check that you are passing a 'X-API-Key' on your header."
         )
 
-
-async def api_key_auth_admin(x_api_key: str = Depends(X_API_KEY)):
-    apikey = await APIKey.objects.filter(apikey=x_api_key).get()
-    os.environ['API-KEY'] = apikey.apikey
-    if x_api_key != os.environ['API-KEY']:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid API Key. Check that you are passing a 'X-API-Key' on your header."
-        )
+# @router.get("/")
+# async def user_list():
+#     userlist = await User.objects.all()
+#     return userlist
 
 
-@router.post("/add_user")
-async def create_user(username, email, password):
-    try:
-        valid_email = validate_email(email)
-        email = valid_email.email
-    except EmailNotValidError as e:
-        return f"L'email inserita non è valida: {e}"
-
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    await User.objects.create(username=username, email=email, hashed_password=hashed_password)
-    return f"Utente {username} è stato creato"
-
-
-@router.post("/login")
-async def user_login(username: str, password: str):
-    user = await User.objects.filter(username=username).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_FORBIDDEN, detail="Credenziali errate")
-    if not user.check_password(password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenziali errate")
-    if not user.check_active():
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Utente non ancora autorizzato")
-    try:
-        existing_token = await APIKey.objects.filter(id_user=user.id).first()
-        if existing_token.id_user == user.id:
-            return {"detail": "Login effettuato con successo, chiave esistente", "X-API-Key": existing_token.apikey}
-    except ormar.exceptions.NoMatch:
-        token = secrets.token_urlsafe(16)
-        await APIKey.objects.create(apikey=token, id_user=user.id, created_at=datetime.now())
-        return {"detail": "Login effettuato con successo", "X-API-Key": token}
-
-
-
-@router.post("/logout")
-async def remove_key(username: str):
-    user_to_remove = await User.objects.filter(username=username).first()
-    deleted_token = await APIKey.objects.delete(id_user=user_to_remove.id)
-    return f"la chiave {deleted_token}  dell'utente {user_to_remove.username} è stata eliminata"
+# async def api_key_auth_admin(x_api_key: str = Depends(X_API_KEY)):
+#     apikey = await APIKey.objects.filter(apikey=x_api_key).get()
+#     os.environ['API-KEY'] = apikey.apikey
+#     if x_api_key != os.environ['API-KEY']:
+#         raise HTTPException(
+#             status_code=401,
+#             detail="Invalid API Key. Check that you are passing a 'X-API-Key' on your header."
+#         )
+#
+#
+# @router.post("/add_user")
+# async def create_user(username, email, password):
+#     try:
+#         valid_email = validate_email(email)
+#         email = valid_email.email
+#     except EmailNotValidError as e:
+#         return f"L'email inserita non è valida: {e}"
+#
+#     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+#     await User.objects.create(username=username, email=email, hashed_password=hashed_password)
+#     return f"Utente {username} è stato creato"
+#
+#
+# @router.post("/login")
+# async def user_login(username: str, password: str):
+#     user = await User.objects.filter(username=username).first()
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_401_FORBIDDEN, detail="Credenziali errate")
+#     if not user.check_password(password):
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenziali errate")
+#     if not user.check_active():
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Utente non ancora autorizzato")
+#     try:
+#         existing_token = await APIKey.objects.filter(id_user=user.id).first()
+#         if existing_token.id_user == user.id:
+#             return {"detail": "Login effettuato con successo, chiave esistente", "X-API-Key": existing_token.apikey}
+#     except ormar.exceptions.NoMatch:
+#         token = secrets.token_urlsafe(16)
+#         await APIKey.objects.create(apikey=token, id_user=user.id, created_at=datetime.now())
+#         return {"detail": "Login effettuato con successo", "X-API-Key": token}
+#
+#
+#
+# @router.post("/logout")
+# async def remove_key(username: str):
+#     user_to_remove = await User.objects.filter(username=username).first()
+#     deleted_token = await APIKey.objects.delete(id_user=user_to_remove.id)
+#     return f"la chiave {deleted_token}  dell'utente {user_to_remove.username} è stata eliminata"
