3e275ac95d1afdc22ea2e1da3930682dbb6d4ecf
halon176 <acc.github@halon.cc>
2023-04-04 14:10:21 +0200

    formatting

--- src/database.py
+++ src/database.py
@@ -5,5 +5,3 @@ from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 metadata = sqlalchemy.MetaData()
 database = databases.Database(f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-
-
